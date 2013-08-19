1961007ca117e704a8e0e414cc720fb7cd717f7d
骑兵程序员 <hmjlr123@gmail.com>
2013-08-19 19:02:24 +0800

    开始补上漏掉的单元测试,下班回家再干

--- app/test/e2e/scenarios.js
+++ app/test/e2e/scenarios.js
@@ -7,9 +7,22 @@ describe('HttpMock', function () {
     beforeEach(function () {
       browser().navigateTo('index.html');
     });
-
-    it('列表正常显示', function () {
-      expect(repeater('.nav-list li').count()).toBe(3)
+    it('默认加载mock列表页', function () {
+      expect(element('.nav-list').count()).toBe(1);
+      expect(browser().location().url()).toBe('/mocks');
+    });
+    it('没有数据时,列表为空', function () {
+      expect(repeater('.nav-list li').count()).toBe(0);
+    });
+    it('创建mock', function () {
+      browser().navigateTo('#/mocks/update/');
+      input('mock.name').enter('www.httpmocktest.com');
+      element('[type="submit"]').click();
+      expect(repeater('.nav-list li').count()).toBe(1);
+      expect(element('table:eq(0) td:eq(0)').text()).toEqual('www.httpmocktest.com');
+    });
+    it('xxx', function () {
+      pause();
     });
   });
 });
